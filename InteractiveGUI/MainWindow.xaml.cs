a3130239566ad35a87687ff58c1210e29839dad4
Nikolaj Bruun-Hansen <nico9908@edu.eal.dk>
2019-02-02 22:46:06 +0100

    fix til buttons

--- InteractiveGUI/MainWindow.xaml.cs
+++ InteractiveGUI/MainWindow.xaml.cs
@@ -27,21 +27,37 @@ namespace InteractiveGUI
         }
         Controller cont = Controller.GetInstance();
 
-        private void Button_Click(object sender, RoutedEventArgs e)
+        private void Btn_NewPerson(object sender, RoutedEventArgs e)
         {
             cont.AddPerson();
+     //     AgeTextBox.Text = "0";
             FirstNameTextBox.IsEnabled = true;
             LastNameTextBox.IsEnabled = true;
             AgeTextBox.IsEnabled = true;
             TelephoneNoTextBox.IsEnabled = true;
+            Up.IsEnabled = true;
+            Down.IsEnabled = true;
+            Delete_Person.IsEnabled = true;
 
-            FirstNameTextBox.Text = null;
-            LastNameTextBox.Text = null;
-            AgeTextBox.Text = null;
-            TelephoneNoTextBox = null;
+     //     FirstNameTextBox.Text = null;
+     //     LastNameTextBox.Text = null;
+     //     AgeTextBox.Text = null;
+     //     TelephoneNoTextBox = null;
 
             Person_Count.Text = "Person Count: " + cont.PersonCount;
             Index.Text = "Index: " + cont.PersonIndex;
+        }
+        private void Btn_DeletePerson(object sender, RoutedEventArgs e)
+        {
+            cont.DeletePerson();
+        }
+        private void Btn_Up(object sender, RoutedEventArgs e)
+        {
+            cont.NextPerson();
+        }
+        private void Btn_Down(object sender, RoutedEventArgs e)
+        {
+            cont.NextPerson();
         }
     }
 }
